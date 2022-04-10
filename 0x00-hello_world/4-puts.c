ab972d2a19aec0a94f83e43e6f8ab3214820f9b4
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-10 04:37:01 -0700

    Task 4 Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,2 @@
-#!/bin/bash
-gcc -E -o c
-#include <studio.h>
-
-/**
- * main - Entr point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+"Programming is like building a multilingual puzzle
