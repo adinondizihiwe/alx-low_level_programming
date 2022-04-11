b6bdca75b931c725e88f7a9b95d6c1781e446f14
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-11 04:50:55 -0700

    my commit message

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
@@ -19,7 +19,7 @@ continue;
 putchar (c);
 }
 
-putchar('\n');
+putchar ('\n');
 
 
 return (0);
