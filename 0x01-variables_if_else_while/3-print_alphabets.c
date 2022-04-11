cc9f1dcb5c2aff4748e4360db40e9d535153d22c
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-11 04:22:45 -0700

    my commit message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
  * Main - Entry point
- *
- * Description: Print lower and uppercase alphabets on the same line
- * Return 0;
+ * Return 0 (Success/correct)
  */
 int main(void)
 {
 int c;
 
 for (c = 0; c < 26; ++c)
+{
 putchar('a' +  c);
+}
 for (c = 0; c < 26; ++c)
+{
 putchar('A' + c);
-
+}
 putchar('\n');
 
 return (0);
