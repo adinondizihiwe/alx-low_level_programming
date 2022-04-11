5db1dedf8f2eca742da05135a610843a9604ea58
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-11 05:45:30 -0700

    commit message

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,15 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: Print numbers 0 to 9 on one line
- * Return: 0
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 int i;
-for (i = 0; i <= 9; ++i)
-putchar('0' + i);
 
+for (i = 48; i <=57; i++)
+{
+putchar(i);
+}
 putchar('\n'); 
 
 return (0);
