c3957c4004766aa06d5fe020ff7e320ee796ef67
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-14 00:27:36 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - couses an infinite loop
+ * Return: Always return 0 (Success/correct)
+ */
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+  /*
+   * while (i < 10)
+   * {
+   * putchar(i);
+   * }
+   */
+printf("Infinite loop avoided \\o/\n");
+return (0);
+}
