b75200f1d9cfbe780523e68b69d2d6484226cb81
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-14 07:41:44 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,22 @@
 
 /**
  * main - check the code.
+ * @c: print int
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-int main(void)
-{
-    char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
