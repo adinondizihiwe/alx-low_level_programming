ca9403f1ff0e3ae4f677f74230ef2553b3e9ad0e
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-26 07:46:07 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,16 @@
  */
 
 void print_rev(char *s)
-{int i;
+{
+int i;
 
 i = 0;
 while (s[i] != '\0')
 {
 i++;
 }
-for (i = 1 - 1 ; i >= 0; i--)
+
+for (i = i - 1 ; i >= 0; i--)
 {
 _putchar (s[i]);
 }
