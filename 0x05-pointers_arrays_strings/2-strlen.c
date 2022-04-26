74dd3f88fae34f76648fa70130e29f7ddc943492
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-26 02:33:00 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - length of a string
+ * @s: A point to an int that will be changed/updated
+ *
+ * Return: void that means our answer is right
+ */
+
+int _strlen(char *s)
+{
+int u;
+
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+
+
