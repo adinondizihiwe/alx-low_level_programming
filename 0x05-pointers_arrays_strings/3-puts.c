e3b0d7753a5a51f3ded1901839294153fd30483e
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-27 00:50:41 -0700

    Task 2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * _puts - print a string
+ * @str: A pointer to an int that will be changed
  *
- * Return: Always 0.
+ * Return: void meaning that the answer is right
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+char *c;
+char *str;
+
+str =  "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
 }
