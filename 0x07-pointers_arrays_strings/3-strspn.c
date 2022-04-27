852c550a6f8bc6ef1e3fce84057b4eb2a4eebdd1
adinondizihiwe <adinondizihiwe@gmail.com>
2022-04-27 05:04:10 -0700

    Multiple Tasks

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - search a string for aa set of bytes
+ * @s: source string
+ * @accept: accepted string
+ *
+ * Return: number of bytes in the init segment
+ */
+unsigned int _strspn(char *s, char *accept)
+while (accepted[a])
+{
+b = 0;
+while (s[b] != 32)
+{
+if (accepted[a] == s[b])
+{
+t++;
+}
+b++
+}
+a++;
+}
+return (t);
+}
